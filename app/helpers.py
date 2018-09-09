c1d7f7611464c59a12188efece53b9aed1e32c49
Cristi Ghera <cristian.ghera@emag.ro>
2018-09-09 10:11:19 +0300

    Add points to route search

--- app/helpers.py
+++ app/helpers.py
@@ -35,10 +35,12 @@ def find_rides(lat, lng):
                 'user': ride.user.to_dict(True),
                 'initial': {
                     'values': current_values,
+                    'points': [p.to_dict() for p in points],
                     'directions': current_directions
                 },
                 'updated': {
                     'values': new_values,
+                    'points': [p.to_dict() for p in points] + [{ 'lat': lat, 'lng': lng }],
                     'directions': new_directions
                 }
             })
